200bf094dbcd5351a480c2a6642152cdcfe8b611
aditya11a <99800286+aditya11a@users.noreply.github.com>
2024-09-07 23:35:54 +0530

    Add files via upload

---
+++ OOP JAVA/ASSIGNMENTS/Day3 Assignment/Short/shortb.java
@@ -0,0 +1,11 @@
+public class shortb
+{
+	
+	public static void main(String[]args)
+	{
+		System.out.println(Short.BYTES);
+	
+	}
+		
+	
+}
---
+++ OOP JAVA/ASSIGNMENTS/Day3 Assignment/Short/shorte.java
@@ -0,0 +1,9 @@
+public class shorte
+{
+	public static void main(String args[])
+	{
+		String strNumber="10";
+		short adi=Short.parseShort(strNumber);
+		System.out.println(adi);
+	}
+}
